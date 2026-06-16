406e424907e6cbf9e901e773a762accec5d363f1
Earl Batenga <ebatengs@gmail.com>
2026-06-16 19:16:25 +0800

    Removed unused script

--- lecture_2.js
+++
@@ -1,9 +0,0 @@
-const myName = "Adam";
-const greet = (name) =>{
- return `OHAYOOO FRIEND, ${name}! I MISS YOU PEEPS`;
- };
- const classmates=["Jackie", "Jana", "Yator", "Jake", "Jaydee"];
- classmates.map(name=>greet(name));
- classmates.forEach(name => {
-     console.log(`%c${greet(name)}`, "color: #8d1ce5; font-weight: bold;");
- });
