cb4d69adc2663329e325ef575d82b656950e73d3
adamalexisyahya-ops <adamalexis.yahya@gmail.com>
2026-06-23 19:21:07 +0800

    ascascasc

---
+++ menu.js
@@ -0,0 +1,26 @@
+[
+  {
+    "id": "1",
+    "category": "Hot Drinks",
+    "name": "Americano",
+    "price": "120",
+    "desc": "Bold and strong black coffee brewed with espresso shots.",
+    "image": "https://images.unsplash.com/photo-1551030173-122aabc4489c?auto=format&fit=crop&w=400&q=80"
+  },
+  {
+    "id": "2",
+    "category": "Hot Drinks",
+    "name": "Cappuccino",
+    "price": "150",
+    "desc": "Classic Italian coffee with equal parts espresso, steamed milk, and foam.",
+    "image": "https://images.unsplash.com/photo-1534778101976-62847782c213?auto=format&fit=crop&w=400&q=80"
+  },
+  {
+    "id": "3",
+    "category": "Hot Drinks",
+    "name": "Latte",
+    "price": "160",
+    "desc": "Smooth espresso blended with creamy steamed milk.",
+    "image": "https://images.unsplash.com/photo-1593443320739-77f74939d0da?q=80&w=736&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+  }
+]
